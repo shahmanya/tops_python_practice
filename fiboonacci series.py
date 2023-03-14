60460f4cedc2a088114bd797cea8adc43810bd3b
shahmanya <manyashah3107@gmail.com>
2023-03-14 16:49:29 +0530

    third attempt

---
+++ fiboonacci series.py
@@ -0,0 +1,11 @@
+num=int(input("Enter number:"))
+n1,n2=0,1
+print("Fiboonaci Series:",n1,n2, end=" ")
+for i in range(2,num):
+    n3=n1+n2
+    n1=n2
+    n2=n3
+    print(n3, end=" ")
+print()
+    
+    
