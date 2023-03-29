6574bd36e14e3e8602dbb76ed0d263d36f1eeef0
shahmanya <manyashah3107@gmail.com>
2023-03-29 21:50:25 +0530

    Seventh Attempt

--- tkinterdemo.py
+++ tkinterdemo.py
@@ -28,8 +28,71 @@ def insert_data():
         e_email.delete(0,"end")
         e_mobile.delete(0,"end")
         msg.showinfo("Insert Status","Data Inserted Successfully")
-    
 
+#Search data to table in database by id
+def search_data():
+    e_fname.delete(0,"end")
+    e_lname.delete(0,"end")
+    e_email.delete(0,"end")
+    e_mobile.delete(0,"end")
+    if e_id.get()=="":
+        msg.showinfo("Search Status","ID is madatory for Search")
+    else:
+        conn=create_conn()
+        cursor=conn.cursor()
+        query="select * from student where id=%s"
+        args=(e_id.get(),)
+        cursor.execute(query,args)
+        row=cursor.fetchall()
+        if row:
+            for i in row:
+                e_fname.insert(0,i[1])
+                e_lname.insert(0,i[2])
+                e_email.insert(0,i[3])
+                e_mobile.insert(0,i[4])
+        else:
+            msg.showinfo("Search Status","ID is not found")
+        conn.close()
+
+#update data
+def update_data():
+    if e_fname.get()=="" or e_lname.get()=="" or e_email=="" or e_mobile.get()=="" or e_id.get()=="":
+        msg.showinfo("Update Status","All fields are mandatory")
+    else:
+        conn=create_conn()
+        cursor=conn.cursor()
+        query="update student set fname=%s,lname=%s,email=%s,mobile=%s where id=%s"
+        args=(e_fname.get(),e_lname.get(),e_email.get(),e_mobile.get(),e_id.get())
+        cursor.execute(query,args)
+        conn.commit()
+        conn.close()
+        e_id.delete(0,"end")
+        e_fname.delete(0,"end")
+        e_lname.delete(0,"end")
+        e_email.delete(0,"end")
+        e_mobile.delete(0,"end")
+        msg.showinfo("Update Status","Data Updated Successfully")
+
+#Delete data
+def delete_data():
+    if e_id.get()=="":
+        msg.showinfo("Delete Status","Id is mandatory")
+    else:
+        conn=create_conn()
+        cursor=conn.cursor()
+        query="delete from student where id=%s"
+        args=(e_id.get(),)
+        cursor.execute(query,args)
+        conn.commit()
+        conn.close()
+        e_id.delete(0,"end")
+        e_fname.delete(0,"end")
+        e_lname.delete(0,"end")
+        e_email.delete(0,"end")
+        e_mobile.delete(0,"end")
+        msg.showinfo("Delete Status","Data Deleted Successfully")
+
+        
 root=Tk() #object of Tk class
 root.geometry("400x380")
 root.title("my tkinter example")
@@ -71,11 +134,11 @@ e_mobile.place(x=200,y=250)
 insert=Button(root,text="INSERT",font=("Arial",10),fg="white",bg="black",command=insert_data)
 insert.place(x=50,y=300)
 
-search=Button(root,text="SEARCH",font=("Arial",10),fg="white",bg="black")
+search=Button(root,text="SEARCH",font=("Arial",10),fg="white",bg="black",command=search_data)
 search.place(x=110,y=300)
 
-update=Button(root,text="UPDATE",font=("Arial",10),fg="white",bg="black")
+update=Button(root,text="UPDATE",font=("Arial",10),fg="white",bg="black",command=update_data)
 update.place(x=178,y=300)
 
-delete=Button(root,text="DELETE",font=("Arial",10),fg="white",bg="black")
+delete=Button(root,text="DELETE",font=("Arial",10),fg="white",bg="black",command=delete_data)
 delete.place(x=244,y=300)
